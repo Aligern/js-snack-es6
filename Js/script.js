7ff4ef08920dd3d348606db30c30e2aaecb712cd
Aligern <sasso.luca93@gmail.com>
2024-03-22 14:18:30 +0100

    first commit

---
+++ Js/script.js
@@ -0,0 +1,68 @@
+/* 
+snack 1
+*/
+const vipTable = [ 
+    {
+        tableName: 'Johnson',
+        guestName: 'Brad Pitt',
+        place: '1'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Johnny Depp',
+    place: '2'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Lady Gaga',
+    place: '3'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Cristiano Ronaldo',
+    place: '4'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Georgina Rodriguez',
+    place: '5'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Chiara Ferragni',
+    place: '6'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'George Clooney',
+    place: '7'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Amal Clooney',
+    place: '8'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Fedez',
+    place: '9'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Amadeus',
+    place: '10'
+},
+{
+    tableName: 'Johnson',
+    guestName: 'Fiorello',
+    place: '11'
+}
+];
+
+for (let key of vipTable){
+    console.log(key.tableName,key.guestName,key.place);
+};
+
+/*
+Snack 2
+*/
