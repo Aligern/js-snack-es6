7f0c4e87006f4e78c75334eb8c2a5027d65e1a5b
Aligern <sasso.luca93@gmail.com>
2024-03-22 14:33:16 +0100

    Milestone 0 of Snack 2

--- Js/script.js
+++ Js/script.js
@@ -3,57 +3,57 @@ snack 1
 */
 const vipTable = [ 
     {
-        tableName: 'Johnson',
+        tableName: 'table: Johnson',
         guestName: 'Brad Pitt',
         place: '1'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Johnny Depp',
     place: '2'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Lady Gaga',
     place: '3'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Cristiano Ronaldo',
     place: '4'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Georgina Rodriguez',
     place: '5'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Chiara Ferragni',
     place: '6'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'George Clooney',
     place: '7'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Amal Clooney',
     place: '8'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Fedez',
     place: '9'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Amadeus',
     place: '10'
 },
 {
-    tableName: 'Johnson',
+    tableName: 'table: Johnson',
     guestName: 'Fiorello',
     place: '11'
 }
@@ -66,3 +66,44 @@ for (let key of vipTable){
 /*
 Snack 2
 */
+
+const nuovaClasse = [
+    {
+        Id: '213',
+        Name: 'Marco della Rovere',
+        Grades: '78',
+    },
+    {
+        Id: '110',
+        Name: 'Paola Cortellessa',
+        Grades: '96',
+    },
+    {
+        Id: '250',
+        Name: 'Andrea Mantegna',
+        Grades: '48',
+    },
+    {
+        Id: '145',
+        Name: 'Gaia Borromini',
+        Grades: '74',
+    },
+    {
+        Id: '196',
+        Name: 'Luigi Grimaldello',
+        Grades: '68',
+    },
+    {
+        Id: '102',
+        Name: 'Piero della Francesca',
+        Grades: '50',
+    },
+    {
+        Id: '120',
+        Name: 'Francesca da Polenta',
+        Grades: '84',
+    },
+];
+
+let nameUppercase = nuovaClasse.map(student => student.Name.toUpperCase());
+console.log(nameUppercase);
