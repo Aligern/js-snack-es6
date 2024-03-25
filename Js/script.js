6aac8f3e59a7716314f3d051042735bec5384f63
Aligern <sasso.luca93@gmail.com>
2024-03-25 13:28:10 +0100

    bicycle array add

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,7 @@
-/* 
-snack 1
-*/
+/*  snack 1 */
+// this is the name of our table:
 let tableName = 'Tavolo Vip';
+// this is the guests list:
 let inviteList = [ 
 'Brad Pitt', 
 'Johnny Depp', 
@@ -15,7 +15,7 @@ let inviteList = [
 'Amadeus', 
 'Fiorello'
 ];
-
+// here we print on console the guests names with their placeholders:
 let placeholder = inviteList.map((guest, i) => {
     return {
         tableName: tableName,
@@ -24,10 +24,8 @@ let placeholder = inviteList.map((guest, i) => {
     }
 });
 console.log(placeholder);
-
-/*
-Snack 2
-*/
+/* Snack 2 */
+// this is our array of students objects:
 const nuovaClasse = [
     {
         Id: '213',
@@ -65,12 +63,36 @@ const nuovaClasse = [
         Grades: '84',
     },
 ];
-// we transform to Uppercases all the students into our array
+// we transform to Uppercases all the students names into our array:
 let nameUppercase = nuovaClasse.map(student => student.Name.toUpperCase());
 console.log(nameUppercase);
-
+// we show on console all the students with a grade value higher than 70:
 let gradesMoreThan70 = nuovaClasse.filter(student => student.Grades > 70);
 console.log(gradesMoreThan70);
-
+// we show on console all the students with a grade value higher than 70 and an ID higher than 120:
 let gradesID = nuovaClasse.filter (student => student.Grades > 70 && student.Id > 120);
 console.log(gradesID);
+/* Snack 3 */
+const bicycle = [
+    {
+        Name: 'rossa',
+        Weight: 15, 
+    },
+    {
+        Name: 'verde',
+        Weight: 35, 
+    },
+    {
+        Name: 'gialla',
+        Weight: 22, 
+    },
+    {
+        Name: 'blu',
+        Weight: 41, 
+    },
+    {
+        Name: 'arancione',
+        Weight: 12, 
+    }
+];
+
